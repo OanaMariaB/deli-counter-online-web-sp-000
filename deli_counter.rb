7fb1c4824cab81fae8297305a9b886c4128d8394
Oana Berentan <oana.berentan@yahoo.com>
2019-05-05 18:06:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 def line(deli)
-if deli.size == 0
+if deli.empty?
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
@@ -10,4 +10,9 @@ if deli.size == 0
     end
     puts current_line
   end
+end
+
+def take_a_number(deli, person)
+  deli << name
+ puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
